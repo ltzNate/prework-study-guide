f1a90f66217ef829d8d8f321e63564c3c8b2b5ca
Nathaniel <nreidcoke@gmail.com>
2022-11-14 00:14:08 -0500

    JavaScript stuff

---
+++ script.js
@@ -0,0 +1,26 @@
+var topics = ['HTML', 'Css', 'Git', 'JavaScript'];
+
+
+
+/*
+if (topic === 'HTML'){
+    
+    console.log("lets study HTML!");
+} 
+else if (topic === 'CSS'){ 
+
+    console.log("let's study CSS!")
+} 
+else if (topic === 'Git'){
+
+    console.log("let's study Git!");
+
+} else if (topic === 'JavaScript'){
+
+    console.log("let's study JavaScript!");
+
+} else {
+    console.log('Please try again.');
+}
+
+*/
