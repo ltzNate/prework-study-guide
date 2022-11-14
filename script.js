178d34f7f5da29121383b32ee9f1bdd3d2baf602
Nathaniel <nreidcoke@gmail.com>
2022-11-14 00:24:26 -0500

    Javascript 2.0

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 var topics = ['HTML', 'Css', 'Git', 'JavaScript'];
-
-
-
-/*
-if (topic === 'HTML'){
-    
-    console.log("lets study HTML!");
-} 
-else if (topic === 'CSS'){ 
-
-    console.log("let's study CSS!")
-} 
-else if (topic === 'Git'){
-
-    console.log("let's study Git!");
-
-} else if (topic === 'JavaScript'){
-
-    console.log("let's study JavaScript!");
-
-} else {
-    console.log('Please try again.');
+function listTopics(){
+    for (var x = 0; x < topics.length; x++){
+        console.log(topics[x]);
 }
 
-*/
+}
+
+function selectTopic(){
+    if (topic === 'HTML'){
+    
+        console.log("lets study HTML!");
+    } 
+    else if (topic === 'CSS'){ 
+    
+        console.log("let's study CSS!")
+    } 
+    else if (topic === 'Git'){
+    
+        console.log("let's study Git!");
+    
+    } else if (topic === 'JavaScript'){
+    
+        console.log("let's study JavaScript!");
+    
+    } else {
+        console.log('Please try again.');
+    }
+    
+    
+    
+}
+
